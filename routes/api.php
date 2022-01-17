64db6c746e40ecd98b2bc333ab94c3c5c3057fdd
rizziqibrahim <rizziqibrahim@gmail.com>
2022-01-17 12:58:20 +0700

    fix login route

--- routes/api.php
+++ routes/api.php
@@ -22,25 +22,10 @@ Route::get('/login', function () {
         'message' => 'Hayoo Antum Mau Ngapain?, halaman ini terlarang buat antum masuki'
     ], 401);
 });
-Route::post('/login',[AuthController::class,'login']);
-Route::post('/nyoba',[AuthController::class,'nyoba']);
+Route::post('/login-email',[AuthController::class,'loginEmail']);
+Route::post('/login-nomor',[AuthController::class,'loginNomor']);
 Route::put('/user/nonaktif',[UserController::class,'nonaktif']);
 Route::put('/tahun-ajaran/nonaktif',[TahunAjaranController::class,'nonaktif']);
-
-//delete data
-Route::delete('/managemen-user/delete',[UserController::class,'delete']);
-Route::delete('/tahunajaran/delete',[TahunAjaranController::class,'delete']);
-Route::delete('/kelas/delete',[KelasController::class,'delete']);
-Route::delete('/managemen-guru/delete',[GuruController::class,'delete']);
-Route::delete('/managemen-jurusan/delete',[JurusanController::class,'delete']);
-Route::delete('/managemen-siswa/delete',[SiswaController::class,'delete']);
-Route::delete('/managemen-wali/delete',[WaliController::class,'delete']);
-Route::delete('/mapel/delete',[MapelController::class,'delete']);
-Route::delete('/mapel-guru/delete',[MapelGuruController::class,'delete']);
-Route::delete('/jadwal/delete',[DetailJadwalController::class,'delete']);
-Route::delete('/hari/delete',[HariController::class,'delete']);
-Route::delete('/jam/delete',[PeriodController::class,'delete']);
-Route::delete('/kelas-siswa/delete',[KelasSiswaController::class,'delete']);
 
 
 
@@ -55,33 +40,12 @@ Route::middleware(['auth:sanctum'])->group(function () {
     Route::middleware('role:admin|guru')->group(function () {
 
             
-        Route::resource('jadwal', DetailJadwalController::class);
-        Route::resource('jam', PeriodController::class);
-        Route::resource('hari', HariController::class);
-        Route::resource('managemen-siswa', SiswaController::class);
-        Route::resource('managemen-user', UserController::class);
-        Route::resource('managemen-guru', GuruController::class);
-        Route::resource('managemen-wali', WaliController::class);
-        Route::resource('kelas', KelasController::class);
-        Route::resource('tahunajaran', TahunAjaranController::class);
-        Route::resource('mapel', MapelController::class);
-        Route::resource('jurusan', JurusanController::class);
-        Route::resource('mapel-guru', MapelGuruController::class);
-        Route::resource('kelas-siswa', KelasSiswaController::class);
-
-        // export & import
-        Route::get('export-user', [UserController::class,'export']);
-        Route::post('import-user', [UserController::class,'import']);
+        
     });
 
 
     Route::middleware('role:guru')->group(function () {
-        Route::resource('jadwal-guru', JadwalController::class);
-        Route::get('jadwal-guru:hari={hari}', [JadwalController::class,'perhari']);
-        Route::get('jadwal-guru:jam={jam}', [JadwalController::class,'perjam']);
-        Route::resource('jadwal-guru', JadwalController::class);
-        Route::resource('absensi', AbsensiController::class);
-        Route::get("absensi/create/{id}", [AbsensiController::class,'create']);
+        
         
     });
 });
