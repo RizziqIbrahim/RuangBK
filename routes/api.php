a432a9d44c9223307bd545487858390730ce4954
rizziqibrahim <rizziqibrahim@gmail.com>
2022-02-14 09:20:27 +0700

    .

--- routes/api.php
+++ routes/api.php
@@ -61,7 +61,7 @@ Route::middleware(['auth:sanctum'])->group(function () {
     Route::post('user/upload/data' , [UserController::class ,'uploadData']);
     Route::post('register-user', [GuruController::class, 'registerUser']);
 
-    Route::get('profile', [UserController::class, 'showLogin']);
+    Route::get('/profile', [UserController::class, 'showLogin']);
 
     Route::get('getSiswa', [GuruController::class ,'getSiswa']);
     //delete
