cc6ca79f09054e25993400750250a2be867b3806
rizziqibrahim <rizziqibrahim@gmail.com>
2022-04-01 13:32:17 +0700

    d

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\User;
 
 class AdminSeeder extends Seeder
 {
@@ -13,6 +14,16 @@ class AdminSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $user = User::create([
+            'nama_user' => "admin",
+            'password' => "12345678",
+            'email' => "admin@gmail.com",
+            'nomor_telp' => 1234567890,
+            'role' => 1,
+            'status' => 1
+
+        ]);
+        $user->assignRole('admin');
+
     }
 }
