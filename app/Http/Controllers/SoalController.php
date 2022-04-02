af388796a00ac63e3b6d7fd322c9e51c9be07e71
rizziqibrahim <rizziqibrahim@gmail.com>
2022-04-02 15:12:15 +0700

    request fix

--- app/Http/Controllers/SoalController.php
+++ app/Http/Controllers/SoalController.php
@@ -110,7 +110,7 @@ class SoalController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(Request $request, $id)
     {
         $request->keywords;
         $request->page;
