0e2a92497e67718e2642c6488cab51540519f9d2
rizziqibrahim <rizziqibrahim@gmail.com>
2022-04-07 10:13:58 +0700

    fix get siswa

--- app/Http/Controllers/GuruController.php
+++ app/Http/Controllers/GuruController.php
@@ -65,16 +65,16 @@ class GuruController extends Controller
         $request->siswa;
         $request->page;
         $request->perpage;
-        $siswa = Siswa::leftjoin('users', 'users.id', '=', 'user_id')->leftjoin('gurus', 'gurus.sekolah', '=', 'siswas.sekolah')
+        $siswa = Siswa::leftjoin('users', 'users.id', '=', 'user_id')->leftjoin('gurus', 'gurus.id', '=', 'siswas.guru_id')
+        ->where('siswas.guru_id', $user->id)
         ->where('siswas.nama_siswa', 'like', '%'.strtolower($request->siswa)."%")
-        ->where('siswas.npsn', 'like', '%'.strtolower($user->npsn)."%")
         ->orderBy("siswas.created_at", 'desc')
         ->paginate($request->perpage, [
             'siswas.id',
             'siswas.user_id',
-            'users.status',
+            'siswas.guru_id',
+            'gurus.nama_guru',
             'siswas.nisn',
-            'siswas.nama_guru',
             'siswas.nama_siswa',
             'users.email',
             'users.nomor_telp',
@@ -158,7 +158,7 @@ class GuruController extends Controller
             // $user = $request->user();
             $siswas = new Siswa;
             $siswas->nama_siswa = $user->nama_user;
-            $siswas->guru_id = $guruProfile->id;
+            $siswas->guru_id = $guru->id;
             $siswas->nisn  = $request->nisn;
             $siswas->sekolah   = $guruProfile->sekolah;
             $siswas->npsn  = $guruProfile->npsn;
