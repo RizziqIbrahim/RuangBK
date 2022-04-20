e8315a7c7ca1ab790274a5b8513d3cc6007feb52
rizziqibrahim <rizziqibrahim@gmail.com>
2022-04-20 10:12:04 +0700

    dsa

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -271,7 +271,18 @@ class AuthController extends Controller
 
                 }
 
+            }else{
+                $siswa = Siswa::where('user_id' , '=', $user->id)->first();
+
+                if($siswa->npsn == ""){
+                    $npsn = "belum terisi";
+                }else{
+                    $npsn ="terisi";
+
+                }
+
             }
+
 
             if($roles[0] == "admin"){
                 $identitas = "terisi";
