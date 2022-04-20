8818b85d12d76cfed263d503f611e47d30a31c57
rizziqibrahim <rizziqibrahim@gmail.com>
2022-04-20 10:19:46 +0700

    s

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -296,7 +296,7 @@ class AuthController extends Controller
                     $identitas ="terisi";
                   
                 }
-              
+              //dsa
             }else{
                 $siswa = Siswa::where('user_id' , '=', $user->id)->first();
         
